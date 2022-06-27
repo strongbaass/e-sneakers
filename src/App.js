6ac8dcfc07a26b53bbe6f9641228b3e826955759
strongbaass <ggg266182@gmail.com>
2022-06-27 18:54:00 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const App = () => {
+  return(
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <svg/>
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul>
+            <li><svg/>50$</li>
+            <li><svg/></li>
+        </ul>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
